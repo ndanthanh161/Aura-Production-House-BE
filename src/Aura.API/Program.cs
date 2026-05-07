204be5f5a0fdc86c80b36fc682f03220ecaf09a3
NguyenDanThanh <Thanhndse183476@fpt.edu.vn>
2026-05-07 23:08:09 +0700

    v1

--- src/Aura.API/Program.cs
+++ src/Aura.API/Program.cs
@@ -116,7 +116,9 @@ app.Use(async (context, next) =>
     context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
     context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
     context.Response.Headers.Append("Referrer-Policy", "strict-origin-when-cross-origin");
-    context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; font-src 'self'; connect-src 'self' https://localhost:7283 https://*.ngrok-free.app;");
+    
+    // Updated CSP to allow Azure domains
+    context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' https://accounts.google.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; font-src 'self'; connect-src 'self' https://*.azurecontainerapps.io https://localhost:7283 https://*.ngrok-free.app;");
     await next();
 });
 
